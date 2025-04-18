e79facf5573d1661aab39a8a028921f8ec819cf1
Osamikoyo <v.pylayev@mail.ru>
2025-04-18 06:22:16 +0500

    add config

---
+++ pkg/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Reqs struct {
+		CreatePollRequestType string `yaml:"create_req_type"`
+		UpdatePollRequestType string `yaml:"update_req_type"`
+	}
+	Topic struct {
+		Request  string `yaml:"req_topic"`
+		Producer string `yaml:"producer_topic"`
+	}
+	KafkaUrl string `yaml:"kafka_url"`
+}
+
+func Init(path string) (*Config, error) {
+	var cfg Config
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("error open file: %v", err)
+	}
+
+	if err = yaml.NewDecoder(file).Decode(&cfg); err != nil {
+		return nil, fmt.Errorf("decode error: %v", err)
+	}
+
+	return &cfg, nil
+}
