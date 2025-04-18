0778fe0dad924897fa517c7ffbe0cf7fef1eb84f
Osamikoyo <v.pylayev@mail.ru>
2025-04-18 06:25:47 +0500

    add repository

---
+++ internal/repository/poll_repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"github.com/Koyo-os/Poll-service/internal/entity"
+	"github.com/Koyo-os/Poll-service/pkg/logger"
+	"gorm.io/gorm"
+)
+
+type PollRepository interface {
+	Add(*entity.Poll) error
+	Update(string, *entity.Poll) error
+}
+
+type PollRepositoryImpl struct {
+	db     *gorm.DB
+	logger *logger.Logger
+}
