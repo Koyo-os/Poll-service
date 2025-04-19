f9494ed6ed36c995b9e0515070d0d06d504f8988
Osamikoyo <v.pylayev@mail.ru>
2025-04-19 07:13:33 +0500

    add poll repository

--- internal/repository/poll_repository.go
+++ internal/repository/poll_repository.go
@@ -2,7 +2,9 @@ package repository
 
 import (
 	"github.com/Koyo-os/Poll-service/internal/entity"
+	"github.com/Koyo-os/Poll-service/pkg/config"
 	"github.com/Koyo-os/Poll-service/pkg/logger"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -14,4 +16,18 @@ type PollRepository interface {
 type PollRepositoryImpl struct {
 	db     *gorm.DB
 	logger *logger.Logger
+}
+
+func Init(cfg *config.Config) (PollRepository, error) {
+	db, err := gorm.Open(sqlite.Open(cfg.DSN))
+	if err != nil {
+		return nil, err
+	}
+
+	db.AutoMigrate(&entity.Poll{})
+
+	return &PollRepositoryImpl{
+		db:     db,
+		logger: logger.Init(),
+	}, nil
 }
