bb9e3e948c51728cf4132cffc089e07fbe504213
Osamikoyo <v.pylayev@mail.ru>
2025-04-19 07:08:36 +0500

    update poll

--- go.mod
+++ go.mod
@@ -8,10 +8,15 @@ require (
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.28 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.47 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.7 // indirect
+	gorm.io/driver/sqlite v1.5.7 // indirect
 	gorm.io/gorm v1.25.12 // indirect
 )
--- internal/entity/poll.go
+++ internal/entity/poll.go
@@ -1,17 +1,28 @@
 package entity
 
-import "github.com/google/uuid"
+import (
+	"time"
 
-type Poll struct {
-	ID      uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
-	Type    string    `json:"type"`
-	Payload any       `json:"payload"`
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Field struct {
+	ID      uint      `gorm:"primaryKey;autoIncrement"`
+	PollID  uuid.UUID `gorm:"type:uuid"`
+	Desc    string    `json:"desc"`
+	Procent float32   `json:"procent"`
 }
 
-func NewPoll(Type string, payload any) *Poll {
-	return &Poll{
-		Type:    Type,
-		Payload: payload,
-		ID:      uuid.New(),
-	}
+type Poll struct {
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	CreatedAt time.Time `json:"created_at"`
+	AuthorID  string    `json:"author_id"`
+	Desc      string    `json:"desc"`
+	Fields    []Field   `json:"fields" gorm:"foreignKey:PollID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
+
+func (p *Poll) BeforeCreate(tx *gorm.DB) (err error) {
+	p.ID = uuid.New()
+	return
 }
