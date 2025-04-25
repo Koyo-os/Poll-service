7beaed0f7744c908de02c62207dabedf7ed3c036
Osamikoyo <v.pylayev@mail.ru>
2025-04-25 06:56:37 +0500

    update init func for repo

--- internal/repository/poll_repository.go
+++ internal/repository/poll_repository.go
@@ -1,14 +1,8 @@
 package repository
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/Koyo-os/Poll-service/internal/entity"
-	"github.com/Koyo-os/Poll-service/pkg/config"
 	"github.com/Koyo-os/Poll-service/pkg/logger"
-	"go.uber.org/zap"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
@@ -22,36 +16,9 @@ type PollRepositoryImpl struct {
 	logger *logger.Logger
 }
 
-func Init(cfg *config.Config) (PollRepository, error) {
-	logger := logger.Init()
-
-	logger.Info("connect to sqlite in " + cfg.Dsn)
-
-	if _, err := os.Stat(cfg.Dsn); os.IsNotExist(err) {
-		file, err := os.Create(cfg.Dsn)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create SQLite file: %w", err)
-		}
-
-		logger.Info("created database file")
-
-		file.Close()
-	}
-
-	db, err := gorm.Open(sqlite.Open(cfg.Dsn))
-	if err != nil {
-		logger.Error("error connect to db", zap.Error(err))
-		return nil, err
-	}
-
-	logger.Info("connected to db")
-
-	db.AutoMigrate(&entity.Poll{})
-
-	logger.Info("migrate")
-
+func Init(db *gorm.DB, logger *logger.Logger) PollRepository {
 	return &PollRepositoryImpl{
 		db:     db,
 		logger: logger,
-	}, nil
+	}
 }
