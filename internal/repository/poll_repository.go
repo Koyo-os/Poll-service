7db1f42b93f3befa5e90087683ce4e9211418f50
Osamikoyo <v.pylayev@mail.ru>
2025-04-28 06:29:09 +0500

    update repo

--- internal/repository/poll_repository.go
+++ internal/repository/poll_repository.go
@@ -1,22 +1,16 @@
 package repository
 
 import (
-	"github.com/Koyo-os/Poll-service/internal/entity"
 	"github.com/Koyo-os/Poll-service/pkg/logger"
 	"gorm.io/gorm"
 )
-
-type PollRepository interface {
-	Add(*entity.Poll) error
-	Update(string, *entity.Poll) error
-}
 
 type PollRepositoryImpl struct {
 	db     *gorm.DB
 	logger *logger.Logger
 }
 
-func Init(db *gorm.DB, logger *logger.Logger) PollRepository {
+func Init(db *gorm.DB, logger *logger.Logger) *PollRepositoryImpl {
 	return &PollRepositoryImpl{
 		db:     db,
 		logger: logger,
