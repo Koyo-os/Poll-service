aaf933ef3fcf76d64cbd0164994892fcc972b912
Osamikoyo <v.pylayev@mail.ru>
2025-05-03 06:26:48 +0500

    update interfaces

--- internal/service/interfaces.go
+++ internal/service/interfaces.go
@@ -1,14 +1,24 @@
 package service
 
-import "github.com/Koyo-os/Poll-service/internal/entity"
+import (
+	"context"
+
+	"github.com/Koyo-os/Poll-service/internal/entity"
+	"github.com/google/uuid"
+)
 
 type (
 	PollRepository interface {
 		Add(*entity.Poll) error
-		Update(string, *entity.Poll) error
+		GetOne(uuid.UUID) (*entity.Poll, error)
+		Update(uuid.UUID, *entity.Poll) error
 	}
 
 	Publisher interface {
 		Publish(any, string) error
+	}
+
+	Casher interface {
+		DoCashing(ctx context.Context, key string, payload any) error // payload must to be pointer
 	}
 )
