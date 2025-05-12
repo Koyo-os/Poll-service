5c3d5fb285beeaf99e36b52718ea6d6acac28b48
Osamikoyo <v.pylayev@mail.ru>
2025-05-12 06:08:58 +0500

    add deletepull client

---
+++ pkg/deletepull/client.go
@@ -0,0 +1,24 @@
+package deletepull
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type DeletePullClient struct {
+	reqChan chan Request
+}
+
+func InitClient(reqChan chan Request) *DeletePullClient {
+	return &DeletePullClient{
+		reqChan: reqChan,
+	}
+}
+
+func (d *DeletePullClient) AddDeleteWaiter(wait time.Duration, id uuid.UUID) {
+	d.reqChan <- Request{
+		WaitTime:  wait,
+		PurposeID: id,
+	}
+}
