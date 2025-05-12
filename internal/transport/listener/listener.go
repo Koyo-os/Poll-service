37352524d4d8d966aa6ebbe0bcd3058f48087c38
Osamikoyo <v.pylayev@mail.ru>
2025-05-12 06:05:08 +0500

    update listener

--- internal/transport/listener/listener.go
+++ internal/transport/listener/listener.go
@@ -3,23 +3,29 @@ package listener
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	"github.com/Koyo-os/Poll-service/internal/entity"
 	"github.com/Koyo-os/Poll-service/internal/service"
 	"github.com/Koyo-os/Poll-service/pkg/config"
 	"github.com/Koyo-os/Poll-service/pkg/logger"
-
+	"github.com/Koyo-os/Poll-service/pkg/metrics"
 	"go.uber.org/zap"
 )
 
 type Listener struct {
 	inputChan chan entity.Event
 	logger    *logger.Logger
-	service   service.PollService
+	service   *service.PollServiceImpl
 	cfg       *config.Config
 }
 
-func Init(inputChan chan entity.Event, logger *logger.Logger, cfg *config.Config, service service.PollService) *Listener {
+func Init(
+	inputChan chan entity.Event,
+	logger *logger.Logger,
+	cfg *config.Config,
+	service *service.PollServiceImpl,
+) *Listener {
 	return &Listener{
 		inputChan: inputChan,
 		service:   service,
@@ -32,27 +38,48 @@ func (list *Listener) Listen(ctx context.Context) {
 	for {
 		select {
 		case event := <-list.inputChan:
-			if event.Type == list.cfg.Reqs.CreatePollRequestType {
-				var poll entity.Poll
+			then := time.Now()
 
-				if err := json.Unmarshal(event.Payload, &poll); err != nil {
-					list.logger.Error("error unmarshal poll", zap.Error(err))
-				}
+			var poll entity.Poll
 
+			if err := json.Unmarshal(event.Payload, &poll); err != nil {
+				list.logger.Error("error unmarshal poll", zap.Error(err))
+				metrics.EventProcessingErrors.WithLabelValues(event.Type).Inc()
+				continue
+			}
+
+			switch event.Type {
+			case list.cfg.Reqs.CreatePollRequestType:
 				if err := list.service.Add(&poll); err != nil {
 					list.logger.Error("error add poll to db", zap.Error(err))
+					metrics.EventProcessingErrors.WithLabelValues(event.Type).Inc()
 				}
-			} else if event.Type == list.cfg.Reqs.UpdatePollRequestType {
-				var poll entity.Poll
+				lag := time.Since(then)
+				metrics.EventProcessingTime.WithLabelValues(event.Type).Observe(float64(lag))
 
-				if err := json.Unmarshal(event.Payload, &poll); err != nil {
-					list.logger.Error("error unmarshal poll", zap.Error(err))
-				}
-
+			case list.cfg.Reqs.UpdatePollRequestType:
 				if err := list.service.Update(poll.ID.String(), &poll); err != nil {
 					list.logger.Error("error update poll", zap.Error(err))
+
+					metrics.EventProcessingErrors.WithLabelValues(event.Type).Inc()
 				}
-			} else {
+				lag := time.Since(then)
+				metrics.EventProcessingTime.WithLabelValues(event.Type).Observe(float64(lag))
+
+			case list.cfg.Reqs.SetClosedRequestType:
+				if err := list.service.SetPollClosed(poll.ID.String()); err != nil {
+					list.logger.Error(
+						"error set poll closed",
+						zap.String("poll_id", poll.ID.String()),
+						zap.Error(err),
+					)
+
+					metrics.EventProcessingErrors.WithLabelValues(event.Type).Inc()
+				}
+				lag := time.Since(then)
+				metrics.EventProcessingTime.WithLabelValues(event.Type).Observe(float64(lag))
+
+			default:
 				list.logger.Warn("unknown event type reciewed", zap.String("type", event.Type))
 			}
 
